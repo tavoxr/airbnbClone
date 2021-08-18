f68278a691c466c72370e2c257dbbc6fcc317ad8
tavoxr <tavoxr23@gmail.com>
2021-08-18 13:52:50 -0600

    navigation tabs

---
+++ src/navigation/HomeTabNavigator.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+import HomeScreen from '../screens/Home/index';
+import GuestsScreen from '../screens/Guests';
+
+import { color } from 'react-native-reanimated';
+import Fontisto from 'react-native-vector-icons/Fontisto';
+import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
+import Feather from 'react-native-vector-icons/Feather';
+import EvilIcons from 'react-native-vector-icons/EvilIcons';
+
+const Tab = createBottomTabNavigator();
+
+const HomeTabNavigator = (props)=>{
+    return(
+        <Tab.Navigator >
+            <Tab.Screen
+                name={"Explore"}
+                component={HomeScreen}
+                options={{
+                    tabBarIcon:({color})=>(
+                        <Fontisto name={"search"} size={25} color={color} />
+                    ),
+                    headerShown: false,
+                    tabBarActiveTintColor: "#f15454"
+                }}
+            />
+            <Tab.Screen
+                name={"Saved"}
+                component={GuestsScreen}
+                options={{
+                    tabBarIcon: ({color})=>(
+                        <SimpleLineIcons name="heart" size={25} color={color} />
+                    ),
+                    headerShown: false,
+                    tabBarActiveTintColor: "#f15454"
+                }}
+            />
+            <Tab.Screen
+                name={"Trips"}
+                component={HomeScreen}
+                options={{
+                    tabBarIcon: ({color})=>(
+                        <Feather name="map" size={25} color={color} />
+                    ),
+                    headerShown: false,
+                    tabBarActiveTintColor: "#f15454"
+                    
+                }}
+            />
+            <Tab.Screen
+                name={"Inbox"}
+                component={HomeScreen}
+
+                options={{
+                    tabBarIcon: ({color})=>(
+                        <Feather name="message-square" size={25} color={color} />
+                    ),
+                    headerShown: false,
+                    tabBarActiveTintColor: "#f15454"
+                }}
+            />
+            <Tab.Screen
+                name={"Profile"}
+                component={HomeScreen}
+                options={{
+                    tabBarIcon: ({color})=>(
+                        <EvilIcons name="user" size={35} color={color} />
+                    ),
+                    headerShown: false,
+                    tabBarActiveTintColor: "#f15454"
+                }}
+            />
+
+
+        </Tab.Navigator>
+
+    );
+}
+
+
+export default HomeTabNavigator;
--- src/navigation/Router.js
+++ src/navigation/Router.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native'
-import {createStackNavigator} from '@react-navigation/stack'
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 
 import DestinationSearchScreen from '../screens/DestinationSearch';
+import GuestsScreen from '../screens/Guests';
+
+import HomeTabNavigator from './HomeTabNavigator';
 
 const Stack =  createStackNavigator();
 
@@ -10,13 +13,31 @@ const Router = (props)=>{
     return(
         <NavigationContainer>
             <Stack.Navigator>
+                <Stack.Screen
+                    name={"Home"}
+                    component={HomeTabNavigator}
+                    options={{
+                        headerShown: false
+                    }}
+                />
+
                 <Stack.Screen 
                     name={"Destination Search"}
                     component={DestinationSearchScreen}
                     options={{
                         title: "Search your Destination",
-                        
+                        headerTitleAlign: "center"
                     }}
+                />
+            
+                <Stack.Screen
+                    name={"Guests"}
+                    component={GuestsScreen}
+                    options={{
+                        title: "How many people?",
+                        headerTitleAlign: "center"
+                    }}
+                
                 />
             </Stack.Navigator>
         </NavigationContainer>
