8d7baa46cb2952596dd02fd73d520afdbe818589
tavoxr <tavoxr23@gmail.com>
2021-08-18 11:58:37 -0600

    navigation & gesture-handlers dependencies installed

--- App.js
+++ App.js
@@ -5,41 +5,23 @@
  * @format
  * @flow strict-local
  */
-
+import 'react-native-gesture-handler';
 import React from 'react';
 import type {Node} from 'react';
 import {
   SafeAreaView,
- 
   useColorScheme,
   StyleSheet,
- 
 } from 'react-native';
 
-
-
 import HomeScreen from './src/screens/Home';
-import Post from './src/components/Post'
-import feed from './assets/data/feed'
-import SearchResultsScreen from './src/screens/SearchResults';
-import LocationSearchScreen from './src/screens/LocationSearch';
-import GuestsScreen from './src/screens/Guests';
 
-const post1 = feed[0];
 
 const App: () => Node = () => {
 
-  
-
   return (
     <SafeAreaView style={styles.container}>
-     {/* <HomeScreen/>  */}
-     {/* <Post post={post1} /> */}
-     {/*  <SearchResultsScreen /> */}
-     {/* <LocationSearchScreen/> */}
-     <GuestsScreen/>
-      
-
+      <HomeScreen/> 
     </SafeAreaView>
   );
 };
